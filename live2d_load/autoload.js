48fc56fee218a83457d3601c9972f503bede75f5
ふわふわ <61719039+kokarinn@users.noreply.github.com>
2020-11-16 14:24:23 +0800

    Add files via upload

--- live2d_load/autoload.js
+++ live2d_load/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/kokarinn/cdn@1.0/live2d_load/";
-const tips_path = "https://cdn.jsdelivr.net/gh/kokarinn/cdn@1.0/live2d_load/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/kokarinn/cdn@1.1/live2d_load/";
+const tips_path = "https://cdn.jsdelivr.net/gh/kokarinn/cdn@1.1/live2d_load/";
 //const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
 
@@ -34,8 +34,8 @@ function loadExternalResource(url, type) {
 	]).then(() => {
 		initWidget({
 			waifuPath: tips_path + "waifu-tips.min.json",
-			apiPath: "https://live2d.fghrsh.net/api/",
-			//cdnPath: "https://cdn.jsdelivr.net/gh/kokarinn/cdn@1.0/live2d_api/"
+			apiPath: "https://cos.fuwafuwa.club/live2d_api/",
+			cdnPath: "https://cdn.jsdelivr.net/gh/kokarinn/cdn@1.1/live2d_api/"
 		});
 	});
 console.log(`
